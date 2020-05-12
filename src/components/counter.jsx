b5d4b1763cbbe85d3166eafe5dffb9ac3eaec859
khanhlecodegym <44794251+khanhlecodegym@users.noreply.github.com>
2020-05-12 15:36:26 +0700

    dont

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,43 @@
 import React, { Component } from "react";
 
 export class Counter extends Component {
-  state = {
-    counter: 0,
-    tags: [],
-  };
 
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <h3>Không có tag nào tồn tại</h3>;
+    state = {
+        counter: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     }
 
+    // constructor() {
+    //     super();
+    //     // console.log(this);
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        console.log("hehehe");
+
+        console.log(this);
+    }
+
+  render() {
     return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>{this.formatCounter()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
     );
   }
 
-  render() {
-    return <React.Fragment>
-        {this.state.tags.length === 0 && "Vui long tạo mới tags"}
-        {this.renderTags()}
-    </React.Fragment>;
-  }
-
   formatCounter() {
-    const { counter } = this.state;
-    return counter === 0 ? "Zero" : counter;
+      const {counter} = this.state;
+      return counter === 0 ? 'Zero' : counter;
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.counter === 0 ? "warning" : "primary";
+      let classes = 'badge m-2 badge-';
+      classes += this.state.counter === 0 ? 'warning' : 'primary';
 
-    return classes;
+      return classes;
   }
 }
 
